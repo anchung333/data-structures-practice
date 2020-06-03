749f05fba1bae8e49a7617da17270ee36d2a54a7
Andy <xcessivepupil2@gmail.com>
2020-06-02 18:18:41 -0700

    Finish linked lists

---
+++ Notes/Linked Lists/LinkedListPractice.js
@@ -0,0 +1,169 @@
+class myLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  size() {
+    let currentNode = this.head;
+    let size = 0;
+    while (currentNode.next !== null) {
+      size++;
+      currentNode = currentNode.next;
+    }
+    return size;
+  }
+
+  isEmpty() {
+    return this.head === null;
+  }
+
+  valueAt(index) {
+    //retrieve value at specified index...could possibly have an index pointer for each node, but will implement just looping through
+    let count = 0;
+    let currentNode = this.head;
+    while (currentNode.next !== null) {
+      if (index === count) {
+        return currentNode.value;
+      } else {
+        currentNode = currentNode.next;
+        count++;
+      }
+    }
+    return -1; //return -1 if reaching the end and index is not matched
+  }
+
+  //helper method to traverse list and return node at specified index
+  nodeAt(index) {
+    let count = 0;
+    let currentNode = this.head;
+    while (currentNode.next !== null) {
+      if (index === count) {
+        return currentNode;
+      } else {
+        currentNode = currentNode.next;
+        count++;
+      }
+    }
+    return null;
+  }
+
+  //AKA unshift
+  pushFront(value) {
+    //update head with a new node that has the passed in value
+    let newHead = new Node(value);
+    newHead.next = this.head;
+    this.head = newHead;
+  }
+  
+  //AKA shift
+  popFront() {
+    let returnVal = this.head.value;
+    this.head = this.head.next;
+    return returnVal;
+  }
+
+  pushBack(value) {
+    //to implement if not using a tail pointer
+    //could use list.valueAt(list.size()) to make things looks simpler but that would be O(N^2)
+    let currentNode = this.head;
+    while (currentNode.next !== null) {
+      currentNode = currentNode.next;
+    }
+    //now we've reached the tail; assign new tail and have old tail point to new tail
+    let newTail = new Node(value);
+    currentNode.next = newTail;
+  }
+
+  pushBackTail(value) {
+    let oldTail = this.tail;
+    let newTail = new Node(value);
+    oldTail.next = newTail;
+    this.tail = newTail;
+  }
+
+  pop() {
+    let currentNode = this.head;
+    let oldTail = null;
+    //look ahead by 2 to find second to last node
+    while (currentNode.next.next !== null) {
+      currentNode = currentNode.next;
+    }
+    //currentNode should be 2nd to last node; simply reassign next to null to get rid of tail
+    oldTail = currentNode.next;
+    currentNode.next = null;
+    //if using tail pointer
+    //this.tail = currentNode;
+    return oldTail.value;
+  }
+
+  front() {
+    return this.head.value;
+  }
+
+  back() {
+    //if using tail pointer
+    //return this.tail.value;
+    let currentNode = this.head;
+    while (currentNode.next !== null) {
+      currentNode = currentNode.next;
+    }
+    return currentNode.value;
+  }
+
+  //inserting BEFORE the current node located at passed-in index
+  insert(index, value) {
+    let nodeBefore = this.nodeAt(index - 1);
+    let insertNode = new Node(value);
+    insertNode.next = nodeBefore.next;
+    nodeBefore.next = insertNode;
+    if (target === null) {
+      return 'OUT OF BOUNDS';
+    }
+  }
+
+  erase(index) {
+    let nodeBefore = this.nodeAt(index - 1);
+    nodeBefore.next = nodeBefore.next.next;
+  }
+
+  //return nth node from end
+  valueNFromEnd(n) {
+    let targetIndex = this.size() - 1 - n;
+    return this.nodeAt(targetIndex).value;
+  }
+
+  reverse() {
+    let n = 0;
+    let size = this.size();
+    let reversedList = new myLinkedList();
+    let reversedCurrNode = reversedList.head;
+    reversedList.head = new Node(this.valueNFromEnd(n));
+    while (n < size - 1) {
+      n++;
+      reversedCurrNode.next = new Node(this.valueNFromEnd(n));
+      reversedCurrNode = reversedCurrNode.next;
+    }
+    this = reversedList;
+  }
+
+  removeValue(value) {
+    let currentNode = this.head;
+    while (currentNode.next !== null) {
+      if (currentNode.next.value === value) {
+        currentNode.next = currentNode.next.next;
+        return;
+      }
+      currentNode = currentNode.next;
+    }
+    return 'VALUE NOT FOUND';
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
